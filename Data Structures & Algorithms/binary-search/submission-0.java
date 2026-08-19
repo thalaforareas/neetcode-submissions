449ccd6acaa0e00ffb2cf95d1f3b758a9cde62ad
Aswin Raj P urk23cs5033 <aswinraj@karunya.edu.in>
2026-08-19 21:10:55 +0530

    Add: binary-search - submission-0

---
+++ Data Structures & Algorithms/binary-search/submission-0.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int search(int[] ar, int t) {
+        int l=0;
+        int h=ar.length-1;
+        while(l<=h)
+        {
+            int mid=(l+h)/2;
+            if(ar[mid]==t)
+            {
+                return mid;
+            }
+            else if(t<ar[mid])
+            {
+                h=mid-1;
+            }
+            else
+            {
+                l=mid+1;
+            }
+
+        }
+        return -1;
+    }
+}
